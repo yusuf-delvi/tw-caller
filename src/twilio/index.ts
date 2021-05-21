b041f977556d1604ed796c9a68436f16c7855987
yusuf-delvi <yusufmddelvi8@gmail.com>
2021-05-21 14:17:53 +0530

    change url

--- src/twilio/index.ts
+++ src/twilio/index.ts
@@ -6,7 +6,7 @@ class TwilioDevice {
   async init(api_key) {
     return new Promise(async (resolve, reject) => {
       try {
-        const data = await fetch(`http://localhost:3000/api/twilio/voice-token?api_key=${api_key}`);
+        const data = await fetch(`https://89b9a8d4f39e.ngrok.io/browser-call-widget/us-central1/call/generateToken?api_key=${api_key}`);
         const { token } = await data.json();
         const device = new Device(token);
         device.on('ready', () => {
@@ -22,8 +22,7 @@ class TwilioDevice {
   }
 
   async call() {
-    if (this.device)
-      this.device.connect();
+    if (this.device) this.device.connect();
   }
 
   async hangUp() {
